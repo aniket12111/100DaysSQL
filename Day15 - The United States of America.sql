4238e3304a1ede54f02e9e9b090006f1f7da9c38
aniket12111 <60349691+aniket12111@users.noreply.github.com>
2024-07-13 02:59:46 +0530

    Create Day15 - The United States of America.sql

---
+++ Day15 - The United States of America.sql
@@ -0,0 +1,34 @@
+In some poorly designed UI applications, there's often a lack of data input restrictions. For instance, in a free text field for the country, users might input variations such as 'USA,' 'United States of America,' or 'US.'
+Suppose we have survey data from individuals in the USA about their job satisfaction, rated on a scale of 1 to 5. Write a SQL query to count the number of respondents for each rating on the scale. Additionally, include the country name in the format that occurs most frequently in that scale.
+
+**Table: survey (primary key : name)**
+
+column name	datatype
+name 	varchar(10)
+job_satisfaction 	int
+country 	varchar(20)
+ 
+**Expected output:**
+
+job_satisfaction	country	number_of_respondents
+4	USA	3
+5	United States	4
+
+**Solution:**
+with cte as (
+    select job_satisfaction, country, count(1) as cnt from survey
+    group by job_satisfaction, country
+    order by job_satisfaction, cnt
+), 
+
+cte1 as (
+    select *, sum(cnt)over(partition by job_satisfaction) as number_of_respondents
+    , rank()over(partition by job_satisfaction order by cnt desc)as rnk
+    from cte
+)
+
+select job_satisfaction, country, number_of_respondents from cte1
+where rnk=1
+
+**Note: If we do not use order by inside windows function then it will not return running data.
+"sum(cnt)over(partition by job_satisfaction) as number_of_respondents" try this with order by clause.
